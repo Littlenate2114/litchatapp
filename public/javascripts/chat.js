e0b2260a0bef5f67dee9f7ae2982fdd180b3b595
wwd <wishingwelldevelopments@gmail.com>
2018-07-31 18:56:50 +1000

    fac

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -9,6 +9,19 @@ $("#chat-input").keydown(function(event) {
           if ($("#chat-input").val() == "") {
 		  location.reload();
 	  }
+          if ($("#chat-input").val() == "/streamdub") {
+		  $("#hidden").html('<audio autoplay><source src="http://stream.dubstep.fm/64mp3" type="audio/mpeg"></audio>');
+		  $("#title").text("Streaming Music From Dubstep FM");
+
+	  }
+          if ($("#chat-input").val() == "/streamdnb") {
+		  $("#hidden").html('<audio autoplay><source src="http://st8.webradioworld.net:8000/;" type="audio/mpeg"></audio>');
+		  $("#title").text("Streaming Music From Liquid-DNB FM");
+	  }
+          if ($("#chat-input").val() == "/streamphy") {
+		  $("#hidden").html('<audio autoplay><source src="http://psyprog.rupsy.ru:8000/psyprog" type="audio/mpeg"></audio>');
+		  $("#title").text("Streaming Music From Psychedelic FM");
+	  }
           if ($("#chat-input").val() != "") {
               socket.emit("chat-message", $("#chat-input").val());
               $("#chat-input").val("");
