83d81b4c5ed392f7b7a2ed7e77e290cea1585d9c
Littlenate2114 <littlenate2114@gmail.com>
2018-11-14 18:36:27 +1000

    Update chat.js

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,41 +1,3 @@
-// This example uses MediaRecorder to record from a live audio stream,
-// and uses the resulting blob as a source for an audio element.
-//
-// The relevant functions in use are:
-//
-// navigator.mediaDevices.getUserMedia -> to get audio stream from microphone
-// MediaRecorder (constructor) -> create MediaRecorder instance for a stream
-// MediaRecorder.ondataavailable -> event to listen to when the recording is ready
-// MediaRecorder.start -> start recording
-// MediaRecorder.stop -> stop recording (this will generate a blob of data)
-// URL.createObjectURL -> to create a URL from a blob, which we can use as audio src
-var recordButton, stopButton, recorder;
-recordButton = document.getElementById('record');
-  stopButton = document.getElementById('stop');
-$('#record').hide();
-
-
-function startRecording() {
-  recordButton.disabled = true;
-  stopButton.disabled = false;
-
-  recorder.start();
-}
-
-function stopRecording() {
-  recordButton.disabled = false;
-  stopButton.disabled = true;
-
-  // Stopping the recorder will eventually trigger the `dataavailable` event and we can complete the recording process
-  recorder.stop();
-}
-
-function onRecordingReady(e) {
-  var audio = document.getElementById('audio');
-  // e.data contains a blob representing the recording
-  audio.src = URL.createObjectURL(e.data);
-  audio.play();
-}
 var socket = io();
 
 //-----------------------------------------------------------------------------
@@ -80,29 +42,9 @@ $("#chat-input").keydown(function(event) {
 socket.on("chat-message", function(message) {
 	if (message == "/streamdub") {
 		$("#chat-container").append('<audio style="display:none;" id="audio" autoplay><source src="http://stream.dubstep.fm/64mp3" type="audio/mpeg"></audio>');
-	  }
-	else if(message == "/rec") {
-		$('#record').show();
-
-
-window.onload = function () {
-
-  // get audio stream from user's mic
-  navigator.mediaDevices.getUserMedia({
-    audio: true
-  })
-  .then(function (stream) {
-    recordButton.disabled = false;
-    recordButton.addEventListener('click', startRecording);
-    stopButton.addEventListener('click', stopRecording);
-    recorder = new MediaRecorder(stream);
-
-    // listen to dataavailable, which gets triggered whenever we have
-    // an audio blob available
-    recorder.addEventListener('dataavailable', onRecordingReady);
-  });
-};
-	}else{
+	  }else if(message == "/litrad"){
+		 $("#chat-container").append('<audio style="display:none;" id="audio" autoplay><source src="https://github.com/Littlenate2114/litchatapp/blob/master/litrad.m4a?raw=true" type="audio/mpeg"></audio>');
+	  }else{
     $("#chat-container").append("<span style='color:green'>[admin@192.168.1.1 ~]$ </span>" + message + "<br />")
 	  }
 });
